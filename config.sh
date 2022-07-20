b9ae1bd7cf9f47d18bbfd93cd0efc33082b46b92
Pranav-Talmale <67180305+Pranav-Talmale@users.noreply.github.com>
2022-07-20 10:14:04 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export TWRP_BRANCH="twrp-11"
-export DT_LINK="https://github.com/Pranav-Talmale/device_motorola_dubai.git -b twrp-12.1"
+export DT_LINK="https://github.com/Pranav-Talmale/recovery_device_motorola_dubai.git -b 12.1"
 
 export DEVICE="dubai"
 export OEM="motorola"
