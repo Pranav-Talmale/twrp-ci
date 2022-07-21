d548632463ffd0b7eb8affa5265e1770ef6bff29
Pranav-Talmale <67180305+Pranav-Talmale@users.noreply.github.com>
2022-07-21 19:27:21 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export TWRP_BRANCH="twrp-12.1"
-export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_lisa.git -b realme-port"
+export DT_LINK="https://github.com/Pranav-Talmale/android_device_xiaomi_lisa.git -b port-dubai"
 
 export DEVICE="lisa"
 export OEM="xiaomi"
@@ -26,11 +26,13 @@ export OUTPUT="output.zip"
 #OF_USE_PROTON_CLANG=true
 #export CUSTOM_CLANG_FOLDER="clang_13.0.0"
 
-# Custom bootable/recovery repository and gerrit stuff
+# Custom repositories and gerrit stuff
 #export CI_USE_CUSTOM_REPO=true
 #export REPO_LINK="https://github.com/Pranav-Talmale/twrp_android_bootable_recovery.git"
 #export REPO_BRANCH="test"
 export TW_CLONE_SKKK_PATCHES=true
+export TW_CLONE_PATCHED_ENGINE=true
+
 
 # Not Recommended to Change
 export SYNC_PATH="$HOME/work/twrp" # Full (absolute) path.
